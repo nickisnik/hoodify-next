0eeb76141a2bac8d59ec827eca42c0259ecec76b
Nick <nikita.nikolenko@gmail.com>
2022-06-02 20:36:57 +0100

    fixed typos

--- components/index.js
+++ components/index.js
@@ -3,5 +3,5 @@ export { default as ShoppingContent } from '../pages/products';
 export { default as Footer } from './Footer';
 export { default as ShoppingItemsList } from './ShoppingItemsList';
 export { default as ShoppingCart } from './ShoppingCart';
-export { default as Home } from '../pages/Home';
+export { default as Home } from '../pages/home';
 export { default as ProductDetails } from './ProductDetails';
